93bb1d45aa570dd79b428b21bc71ab315d181747
Sam1991 <samuel_03061991@hotmail.com>
2014-12-22 20:48:25 -0400

    noticias-comentarios
    comentarios de facebook

--- EPI/protected/views/noticia/view.php
+++ EPI/protected/views/noticia/view.php
@@ -35,7 +35,11 @@ $this->menu=array(
 
 <?php $ref="http://epi.ubiobio.cl/EPI/index.php?r=noticia/view&amp;id=".$model->no_id; 
 ?>
-<div class="fb-share-button" data-href="<?php  echo $ref;?>" data-layout="button"></div>
+
+<div style="margin-bottom: 50px;text-align: center;">
+<div class="fb-share-button" data-href="<?php  echo $ref;?>" data-layout="button"></div>	
+
+
 
 <!-- Fin-me gusta -->
 
@@ -48,10 +52,17 @@ $this->menu=array(
   data-via="twitterdev">
 Tweettear
 </a>
+</div>
 <script type="text/javascript">
 window.twttr=(function(d,s,id){var t,js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id)){return}js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);return window.twttr||(t={_e:[],ready:function(f){t._e.push(f)}})}(document,"script","twitter-wjs"));
 </script>
 <!-- fin_twittear -->
+
+<!-- comentarios -->
+<div style="text-align:center;">
+<div class="fb-comments" data-href="<?php  echo $ref;?>" data-numposts="5" data-colorscheme="light"></div>
+</div>
+<!-- fin-comentarios -->
 
  <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
